41613e6ec533d829e9e5c975ef41e2d29c9fe118
Ануфриев Павел Васильевич <p.anufriev@nicetu.spb.ru>
2023-08-09 18:31:28 +0300

    Add an initial sample form

---
+++ src/main/java/org/example/HexGUI.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class HexGUI {
+    private JPanel mainPanel;
+    private JTextArea resultTextArea;
+    private JTable table1;
+
+    public void setTable1(){
+        table1.add(new JLabel("LOL"));
+    }
+}
